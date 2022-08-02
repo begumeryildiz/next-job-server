683c1d72634af9dc0ec566660a590ebd4e50067e
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-02 22:07:26 +0200

    created index.routes.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ require("./config")(app);
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 app.use("/api", require("./routes/index.routes"));
-app.use("/api", require("./routes/auth.routes"));
+//app.use("/api", require("./routes/auth.routes"));
                 
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
