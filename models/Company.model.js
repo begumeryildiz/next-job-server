1d5bbd60c61df963116e8bfe847faf840c958b1b
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-02 23:08:39 +0200

    created Company.model.js

---
+++ models/Company.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const companySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    job: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Job',
+    }],
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        type: String
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ["Point"],
+        },
+        coordinates: {
+            type: [Number],
+            index: "2dsphere",
+        },
+        formattedAddress: String,
+    },
+});
+
+companySchema.pre("save", async function (next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: "Point",
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+    };
+    next();
+});
+
+module.exports = model('Company', companySchema);
